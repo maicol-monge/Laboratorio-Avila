e51073f1d40f5ca98cf22e6094f281763aeba1e0
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-11-03 02:13:02 -0600

    Token en endpoints

--- Backend/routes/examen_realizadoRoutes.js
+++ Backend/routes/examen_realizadoRoutes.js
@@ -8,17 +8,18 @@ const {
   exportExamenRealizado,
   exportExamenRealizadoPdf,
 } = require("../controllers/examen_realizadoController");
+const authenticateToken = require("../middlewares/auth");
 const router = express.Router();
 
 // CRUD de exámenes realizados
-router.get("/", getExamenesRealizados);
-router.post("/", addExamenRealizado);
+router.get("/", authenticateToken, getExamenesRealizados);
+router.post("/", authenticateToken, addExamenRealizado);
 // Exportar examen realizado a .docx (debe ir antes de la ruta /:id)
-router.get("/:id/export", exportExamenRealizado);
+router.get("/:id/export", authenticateToken, exportExamenRealizado);
 // Exportar/Imprimir examen realizado a PDF (misma plantilla) usando LibreOffice
-router.get("/:id/export-pdf", exportExamenRealizadoPdf);
-router.get("/:id", getExamenRealizadoById);
-router.put("/:id", updateExamenRealizado);
-router.delete("/:id", deleteExamenRealizado);
+router.get("/:id/export-pdf", authenticateToken, exportExamenRealizadoPdf);
+router.get("/:id", authenticateToken, getExamenRealizadoById);
+router.put("/:id", authenticateToken, updateExamenRealizado);
+router.delete("/:id", authenticateToken, deleteExamenRealizado);
 
 module.exports = router;
